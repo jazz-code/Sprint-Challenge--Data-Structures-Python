413ccadfaef95005a026510959d6661de74b9e60
ren.meta@gmail.com <ren.meta@gmail.com>
2019-12-20 14:40:43 -0500

    implemented binary search on names

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,22 +1,32 @@
 from doubly_linked_list import DoublyLinkedList
 
+# class RingBuffer:
+#     def __init__(self, capacity):
+#         self.capacity = capacity
+#         self.current = None
+#         self.storage = DoublyLinkedList()
+#         self.data = []
 
-class RingBuffer:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.current = None
-        self.storage = DoublyLinkedList()
+#     def append(self, item):
+#         # pass
+#         # when capacity is full append last element
+#         if self.data.length == self.capacity:
+#             # print("item",item)
+#             # self.storage.add_to_head(item)
+#             self.current = 0
+#             self.storage[self.current] = item
+#             self.current = (self.current + 1) % self.capacity
+#         else:
+#             self.storage.add_to_head(item)
 
-    def append(self, item):
-        pass
 
-    def get(self):
-        # Note:  This is the only [] allowed
-        list_buffer_contents = []
+#     def get(self):
+#         # Note:  This is the only [] allowed
+#         list_buffer_contents = []
 
-        # TODO: Your code here
-
-        return list_buffer_contents
+#         # if self.storage is not None:
+#             # print(self.storage.head.value)
+#         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
 
